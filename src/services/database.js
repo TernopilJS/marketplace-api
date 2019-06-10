6267c93dc88e0378989e6049d7ee150555f6f99f
oleh-mryhlod <oleg.mryglod@gmail.com>
2019-06-10 14:23:51 +0300

    fix db safeParams condition

--- src/services/database.js
+++ src/services/database.js
@@ -63,7 +63,7 @@ export function sql(strings, ...options) {
 
 export function safeParams(params) {
   return params.reduce((acc, value) => {
-    if (typeof value === 'undefined') acc.push(value);
+    if (typeof value !== 'undefined') acc.push(value);
 
     return acc;
   }, []);
